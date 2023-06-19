945d626475d405019d906c3d9c771de699ae396b
Peter Dobbins <109819737+Peter-Dobbins@users.noreply.github.com>
2023-06-19 12:13:49 +0100

    Update livener.js

--- livener.js
+++ livener.js
@@ -21,19 +21,33 @@ titleElement.addEventListener("mouseout", function() {
 
 // this is for the scrolling events that brings the writing in from the side
 
+var elements = document.querySelectorAll('.animate');
+var currentIndex = 0;
+var lastScrollTop = 0;
+var delay = 1000; // Adjust the delay value as needed
+
 window.addEventListener('scroll', function() {
-  var elements = document.querySelectorAll('.animate');
-  
-  for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
-    var positionFromTop = element.getBoundingClientRect().top;
-    var windowHeight = window.innerHeight;
+  var st = window.pageYOffset || document.documentElement.scrollTop;
+  var scrollDirection = st > lastScrollTop ? 'down' : 'up';
+  lastScrollTop = st;
+
+  if (scrollDirection === 'down') {
+    var positionFromTop = elements[currentIndex].getBoundingClientRect().top;
     
-    if (positionFromTop - windowHeight <= 0) {
-      element.classList.add('animate-in');
+    if (positionFromTop - window.innerHeight <= 0) {
+      setTimeout(function() {
+        elements[currentIndex].classList.add('animate-in');
+        currentIndex++;
+      }, delay);
+    }
+  } else if (scrollDirection === 'up') {
+    if (currentIndex > 0) {
+      currentIndex--;
+      elements[currentIndex].classList.remove('animate-in');
     }
   }
 });
+
 
 
 
